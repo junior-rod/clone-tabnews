f95ae09ffac1c08dbb7714f49dc12e80ca915739
Junior Rodrigues <102836957+junior-rod@users.noreply.github.com>
2024-06-25 18:11:01 +0000

    Atualização do index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>A vida é um desafio.</h1>
+    return <h1>A vida não é o problema, é batalha, desafio. Cada obstáculo é uma lição, eu anuncio.</h1>
 }
 export default Home;
