27529178a8a5131cf7c77c9f34033228fa99fd9a
Junior Rodrigues <102836957+junior-rod@users.noreply.github.com>
2024-06-27 18:46:09 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
-function Home(){
-    return <h1>A vida não é o problema, é batalha, desafio. Cada obstáculo é uma lição, eu anuncio.</h1>
+function Home() {
+  return (
+    <h1>
+      A vida não é o problema, é batalha, desafio. Cada obstáculo é uma lição,
+      eu anuncio.
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 export default Home;
